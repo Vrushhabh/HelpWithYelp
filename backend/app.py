36ea3927173d7a0c45e328b52eedcc10ee66aa22
Vrushhabh <vpate81@illinois.edu>
2022-11-08 14:08:35 -0600

    added delete review functionality

--- backend/app.py
+++ backend/app.py
@@ -43,15 +43,13 @@ def get_num_of_categories(postal):
     query_str1 = "SELECT a.category, count(a.category) FROM (SELECT * FROM businesses where stars >= 3 and postal ="
     postal_arg = str(postal) + ")"
     query_str2 = " as b inner join (Select * FROM categories1 where business_id in (Select business_id from categories1 where category = 'Restaurants') ) a using (business_id) group by a.category order by count(a.category) desc"
-  
 
     query = query_str1 + postal_arg + query_str2 
-    
-
+    #make cursor to iterate through table
     mycursor = dataBase.cursor()
     mycursor.execute(query) 
 
-    # make json keys to send to frontend
+    # make json keys that correspond with queried  to send to frontend
     row_headers=[x[0] for x in mycursor.description] 
     myresult = mycursor.fetchall()
     json_data=[]
@@ -61,4 +59,23 @@ def get_num_of_categories(postal):
       json_data.append(dict(zip(row_headers,x)))
 
     return json.dumps(json_data)
+          
+# get the number of categories from each zip postal code
+@app.route('/delete-unhelpful-review/<review_id>', methods=["GET"]) #change to post
+def delete_review(review_id):
+    query_str1 = "DELETE FROM reviews WHERE "
+    reviewid_arg = "reviews.review_id like " +" '" + str(review_id) +"'"
+    query = query_str1 + reviewid_arg
+    mycursor = dataBase.cursor()
+    mycursor.execute(query) 
+    print(query)
+
+    return "successfully deleted"
+
+
+
+
+   
+
+
           
