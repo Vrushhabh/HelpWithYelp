8ce55c3575194a0af85a31ce209e793cd355d961
Michael Parekh <mparekh4@illinois.edu>
2022-11-09 18:26:38 -0600

    add insights page

---
+++ frontend/src/pages/insights.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from '../logo.png';
+import '../App.css';
+
+function Insights() {
+  return (
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' alt='logo' />
+        <h1>Help with Yelp</h1>
+        <h2> This page is for the data insights</h2>
+      </header>
+    </div>
+  );
+}
+
+export default Insights;
